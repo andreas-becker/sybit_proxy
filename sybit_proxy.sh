1fe2a2e38f369c2bfe1babbc360c9d4410ab09ba
Andreas Becker <andreas-becker@users.noreply.github.com>
2021-07-16 15:50:26 +0200

    Update sybit_proxy.sh
    changed url

--- sybit_proxy.sh
+++ sybit_proxy.sh
@@ -4,7 +4,7 @@ version="0.1.0"
 repo_name="andreas-becker/sybit_proxy"
 script_name="sybit_proxy.sh"
 
-link=$(curl -L -o /dev/null -w '%{url_effective}' "https://github.com/${repo_name}/releases/latest")
+link=$(curl -L -o /dev/null -w '%{url_effective}' "https://raw.githubusercontent.com/${repo_name}/main/sybit_proxy.sh")
 version=$(basename "$link")
 if [ "$version" == "$script_version" ] || [ "$version" == "latest" ]; then OUTDATED="0"; else OUTDATED="1"; fi
 
@@ -12,7 +12,7 @@ if [ "$OUTDATED" == "1" ]; then
     printf "%s\n" "Updating script..."
     export skip="1"
     (rm "$script_name"
-    curl -s "https://api.github.com/repos/${repo_name}/releases/latest" \
+    curl -s "https://raw.githubusercontent.com/${repo_name}/main/sybit_proxy.sh" \
     | grep browser_download_url \
     | grep "$script_name" \
     | cut -d '"' -f 4 \
